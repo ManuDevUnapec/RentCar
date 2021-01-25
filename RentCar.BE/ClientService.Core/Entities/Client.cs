e0507abce0f99e645844b4e6a4e4667915280728
Manuel De Jesus Guerrero Vasquez <manueldejesusguerrerovasquez@Manuels-MacBook-Pro.local>
2021-01-25 10:16:19 -0400

    Creating the ClientService.Core

---
+++ RentCar.BE/ClientService.Core/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ClientService.Core.Entities
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string IdentificationCard { get; set; }
+        public string CardNumber { get; set; }
+        public int CreditLimit { get; set; }
+        public object MyProperty { get; set; }
+    }
+}
---
+++ RentCar.BE/ClientService.Core/Interfaces/IClientRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using ClientService.Core.Entities;
+
+namespace ClientService.Core.Interfaces
+{
+    public interface IClientRepository : IGenericRepository<Client>
+    {
+    }
+}
