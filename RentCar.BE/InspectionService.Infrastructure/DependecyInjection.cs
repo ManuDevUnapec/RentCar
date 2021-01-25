bfe04fb2c6243bc79707c60e26da551b7870272b
Manuel De Jesus Guerrero Vasquez <manueldejesusguerrerovasquez@Manuels-MacBook-Pro.local>
2021-01-25 19:05:59 -0400

    Adding the InspectionService.Insfrastructure project

---
+++ RentCar.BE/InspectionService.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using InspectionService.Core.Interfaces;
+using InspectionService.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InspectionService.Infrastructure
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IInspectionRepository, InspectionRepository>();
+            return services;
+        }
+    }
+}
