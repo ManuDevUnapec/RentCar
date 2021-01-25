534fe6b4de7cfb232c5f9fd1aac6f19486880de7
Manuel De Jesus Guerrero Vasquez <manueldejesusguerrerovasquez@Manuels-MacBook-Pro.local>
2021-01-25 12:06:16 -0400

    Adding EmployeeService.Infrastructure

--- RentCar.BE/EmployeeService.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EmployeeService.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ RentCar.BE/EmployeeService.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EmployeeService.Core.Interfaces;
+using EmployeeService.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeService.Infrastructure
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+            return services;
+        }
+    }
+}
