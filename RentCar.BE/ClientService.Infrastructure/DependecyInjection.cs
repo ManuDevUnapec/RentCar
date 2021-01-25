22a5144e989df97af093152d83f4d39375d1d232
Manuel De Jesus Guerrero Vasquez <manueldejesusguerrerovasquez@Manuels-MacBook-Pro.local>
2021-01-25 10:33:08 -0400

    Adding the ClientService.Infrastructure

---
+++ RentCar.BE/ClientService.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ClientService.Core.Interfaces;
+using ClientService.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClientService.Infrastructure
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IClientRepository, ClientRepository>();
+            return services;
+        }
+    }
+}
---
+++ RentCar.BE/ClientService.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ClientService.Core.Entities;
+using ClientService.Core.Interfaces;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace ClientService.Infrastructure.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public ClientRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<int> Add(Client entity)
+        {
+            var sql = "INSERT INTO Clients (Name, IdentificationCard, CardNumber, CreditLimit, " +
+                "PersonType, Status) " +
+                "Values (@Name, @IdentificationCard, @CardNumber, @CreditLimit, @PersonType, @Status);";
+
+            try
+            {
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("ClientConnection")))
+                {
+                    connection.Open();
+                    var affectedRows = await connection.ExecuteAsync(sql, entity);
+                    return affectedRows;
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            var sql = "DELETE FROM Clients WHERE ID = @ID;";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("ClientConnection")))
+            {
+                connection.Open();
+                var affectedRows = await connection.ExecuteAsync(sql, new { ID = id });
+                return affectedRows;
+            }
+        }
+
+        public async Task<Client> Get(int id)
+        {
+            var sql = "SELECT * FROM Clients WHERE ID = @ID;";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("ClientConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Client>(sql, new { ID = id });
+                return result.FirstOrDefault();
+            }
+        }
+
+        public async Task<IEnumerable<Client>> GetAll()
+        {
+            try
+            {
+                var sql = "SELECT * FROM Clients;";
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("ClientConnection")))
+                {
+                    connection.Open();
+                    var result = await connection.QueryAsync<Client>(sql);
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                //Log Error
+                return new List<Client>();
+            }
+        }
+
+        public async Task<int> Update(Client entity)
+        {
+            var sql = "UPDATE Clients SET Name = @Name, IdentificationCard = @IdentificationCard, CardNumber = @CardNumber," +
+                "CreditLimit = @CreditLimit, Status = @Status, PersonType = @PersonType" +
+                "WHERE ID = @ID;";
+
+            try
+            {
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("ClientConnection")))
+                {
+                    connection.Open();
+                    var affectedRows = await connection.ExecuteAsync(sql, entity);
+                    return affectedRows;
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
