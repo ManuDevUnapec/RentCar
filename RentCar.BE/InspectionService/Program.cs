b45488ab7428ee65aaf4286828d79233454356a1
Manuel De Jesus Guerrero Vasquez <manueldejesusguerrerovasquez@Manuels-MacBook-Pro.local>
2021-03-02 07:57:09 -0400

    Adding logs for the Inspection Microservice

--- RentCar.BE/InspectionService/Program.cs
+++ RentCar.BE/InspectionService/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace InspectionService
 {
@@ -13,14 +14,26 @@ namespace InspectionService
     {
         public static void Main(string[] args)
         {
+            //Settings for SeriLog
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
